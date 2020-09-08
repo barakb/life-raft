4ae74976f72e25fb4db1ca11d1400b12860c0ddb
Barak Bar Orion <barak.bar@gmail.com>
2020-09-08 11:01:59 +0300

    Add test for deleting old logs from the follower while replicating.

--- src/test/kotlin/org/barak/raft/ServerTest.kt
+++ src/test/kotlin/org/barak/raft/ServerTest.kt
@@ -33,8 +33,28 @@ internal class ServerTest {
         }
     }
 
+    @RepeatedTest(1)
+    fun twoNodesSelectALeaderLeaderEmptyLog() {
+        assertTimeout(Duration.ofSeconds(5)) {
+            runBlocking {
+                InProcessTransport().use {
+                    val server1 = Server(endpoint("server1"), mutableListOf("server2"))
+                    val server2 = Server(endpoint("server2"), mutableListOf("server1"))
+                    delay(3.seconds)
+                    val servers = listOf(server1, server2)
+                    assertTrue(exactlyOneIsLeader(servers))
+                    val leaderLog = servers.find { it.state == State.Leader }!!.log
+                    assertTrue(servers.map { it.log }.all { it == leaderLog })
+
+                    server1.close()
+                    server2.close()
+                }
+            }
+        }
+    }
+
     @RepeatedTest(5)
-    fun twoNodesSelectALeaderLeader() {
+    fun twoNodesSelectALeaderLeaderWithSimpleLogsReplication() {
         assertTimeout(Duration.ofSeconds(5)) {
             runBlocking {
                 InProcessTransport().use {
@@ -43,11 +63,35 @@ internal class ServerTest {
                     val server1 = Server(endpoint("server1"), mutableListOf("server2"), log)
                     val server2 = Server(endpoint("server2"), mutableListOf("server1"))
                     delay(3.seconds)
-//                    val servers = listOf(server1, server2)
-//                    assertTrue(exactlyOneIsLeader(servers))
                     assertTrue(server1.state == State.Leader)
                     assertTrue(server2.log.lastIndex() == 1L)
                     assertTrue(server2.log[1L] == log[1L])
+                    server1.close()
+                    server2.close()
+                }
+            }
+        }
+    }
+
+    @RepeatedTest(5)
+    fun twoNodesSelectALeaderLeaderWithComplexLogsReplication() {
+        assertTimeout(Duration.ofSeconds(5)) {
+            runBlocking {
+                InProcessTransport().use {
+                    val log1 = Log()
+                    log1.push(LogEntry(0, "term0"))
+                    log1.push(LogEntry(1, "term1"))
+                    log1.push(LogEntry(1, "term1"))
+                    val log2 = Log()
+                    log2.push(LogEntry(0, "term0"))
+                    log2.push(LogEntry(0, "term0"))
+                    log2.push(LogEntry(0, "term0"))
+                    log2.push(LogEntry(0, "term0"))
+                    val server1 = Server(endpoint("server1"), mutableListOf("server2"), log1)
+                    val server2 = Server(endpoint("server2"), mutableListOf("server1"), log2)
+                    delay(3.seconds)
+                    assertTrue(server1.state == State.Leader)
+                    assertEquals(server1.log, server2.log)
                     server1.close()
                     server2.close()
                 }
